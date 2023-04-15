5abc14679494f3e4ce6a1c22c1068960bb76c3d6
Anna Esteve <aesteve8@gmail.com>
2023-04-15 17:57:02 +0200

    Ordered countries. Pending: pinia

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -29,7 +29,19 @@ export default {
       const data = await fetch("https://ih-countries-api.herokuapp.com/countries");
       const response = await data.json();
       this.countriesList = response;
-      // console.log(this.countriesList);
+      this._orderCountriesList();
+      console.log(this.countriesList);
+    },
+    _orderCountriesList(){
+      this.countriesList.sort((a, b) => {
+        if (a.name.common < b.name.common) {
+          return -1;
+        }
+        if (a.name.common > b.name.common) {
+          return 1;
+        }
+        return 0;
+      });
     }
   },
   mounted(){
